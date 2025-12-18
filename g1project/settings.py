a431fbcc3202db9d4ca43e07ffdb5acd25db4013
nisha200-creator <nisha70016@gmail.com>
2025-12-18 09:05:54 +0530

    remove password

--- g1project/settings.py
+++ g1project/settings.py
@@ -157,12 +157,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nisha70016@gmail.com'
-EMAIL_HOST_PASSWORD = 'hurmyzkiyqqojthp'
-DEFAULT_FROM_EMAIL = 'nisha70016@gmail.com'
 
 
